64ee2b2b585afcdd7df81dd8945b46e09183a9ca
bertrand AVE <bertrand AVE>
2017-05-07 18:33:47 +0200

    authentification

--- src/main/generated-java/import.sql
+++ src/main/generated-java/import.sql
@@ -1,8 +1,8 @@
 -- add admin user
-INSERT INTO "app_user" ("id", "login", "password", "email", "language", "firstName", "lastName", "enabled")  VALUES (-1, 'admin', 'admin', 'admin@admin.com', 'FR', 'admin', 'admin', 1);
+INSERT INTO "app_user" ("id", "login", "password", "email", "language", "first_name", "last_name", "enabled")  VALUES (-1, 'admin', 'admin', 'admin@admin.com', 'FR', 'admin', 'admin', 1);
 
 -- add admin role (lower case)
-INSERT INTO "app_authority"  VALUES (-1, 'admin');
+INSERT INTO "app_authority" ("id", "name") VALUES (-1, 'admin');
 
 -- link admin role to admin user
-INSERT INTO "app_user_authority"  VALUES (-1, -1, -1);
+INSERT INTO "app_user_authority" ("id", "app_user_id", "app_authority_id") VALUES (-1, -1, -1);
