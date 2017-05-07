509def4b5e72690694d65e48b36b87e0a2c0c9fc
bertrand AVE <bertrand AVE>
2017-05-07 23:13:57 +0200

    authentification

--- src/main/resources/static/assets/js/app.js
+++ src/main/resources/static/assets/js/app.js
@@ -8,11 +8,11 @@
 
 var app = angular.module("mainApp", 
 		["ngResource", /* for REST WS calls */
-		 "ui.router", /* pour découper l'appli en liens */
+		 "ui.router", /* pour d�couper l'appli en liens */
 		 "pascalprecht.translate", /* for label translation */
 		 "ngAnimate", 
 		 "ngSanitize", 
-		 "mgcrea.ngStrap", /* ces 3 dernières libs pour mgcrea */
+		 "mgcrea.ngStrap", /* ces 3 derni�res libs pour mgcrea */
 		 "ngCsv", /* for downloading entites as CSV files, see https://github.com/asafdav/ng-csv */
 		 "http-auth-interceptor" /* permet de faciliter le login d'un user en angularjs */
 ]);
--- src/test/generated-java/com/jaxio/demo/repository/AppParameterRepositoryTest.java
+++ src/test/generated-java/com/jaxio/demo/repository/AppParameterRepositoryTest.java
@@ -17,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
